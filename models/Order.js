111c01e59dc1b9d923e56b748304be239dde8bb3
Abdur-Raqeeb Ajao <ajaorqb@gmail.com>
2020-12-25 01:16:20 +0100

    Chore: removed image field from orderItems

--- models/Order.js
+++ models/Order.js
@@ -20,7 +20,6 @@ const OrderSchema = new mongoose.Schema({
         {
             name: { type: String, required: true },
             qty: { type: Number, required: true },
-            image: { type: String, required: true },
             price: { type: Number, required: true },
             product: {
                 type: mongoose.Schema.ObjectId,

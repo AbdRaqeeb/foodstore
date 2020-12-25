8794dd0f9d2ee8820d8615d2c5b1aac372ac42ea
Abdur-Raqeeb Ajao <ajaorqb@gmail.com>
2020-12-25 07:44:30 +0100

    Chore: removed environment var

--- utils/paystack.js
+++ utils/paystack.js
@@ -1,6 +1,6 @@
 import PayStack from 'paystack-node';
 import 'dotenv/config.js';
 
-const paystack = new PayStack(process.env.PAYSTACK_API_KEY, process.env.NODE_ENV_PAYSTACK);
+const paystack = new PayStack(process.env.PAYSTACK_API_KEY);
 
 export default paystack;
